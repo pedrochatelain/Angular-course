9e17cfc567a9a0b45b0aabdfc445613e96514564
Pedro Chatelain <pedrochatelain@gmail.com>
2022-01-21 14:52:20 -0300

    Last class 137. Cards y botones con íconos

---
+++ pipesApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+// Prime ng
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ButtonModule,
+    CardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
