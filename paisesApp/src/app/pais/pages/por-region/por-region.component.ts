0b678a7631e8a0acdd10127110248de2737e2b57
Pedro Chatelain <pedrochatelain@gmail.com>
2022-01-21 13:27:49 -0300

    Finish countries proyect

--- paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,38 +1,43 @@
 import { Component } from '@angular/core';
+
 import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [`
+  styles: [ `
     button {
       margin-right: 5px;
-    } 
-  `]
+    }
+  
+  `
+  ]
 })
 export class PorRegionComponent {
 
-  regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC']
-  regionActiva: string = ''
-  countries: Country[] = [];
+  regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC',];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor(private paisService: PaisService) { }
+  
+  constructor( private paisService: PaisService ) { }
 
-  activarRegion(region: string) {
-    if (region !== this.regionActiva) {
-      this.regionActiva = region;
-      this.countries = [];
-      this.paisService.getCountriesByRegion(region)
-        .subscribe(countries => {
-          this.countries = countries
-          console.log(countries)
-        })
-    }
+  getClaseCSS( region: string ): string {
+    return (region === this.regionActiva) 
+              ? 'btn btn-primary'
+              : 'btn btn-outline-primary';
   }
 
-  getCSS(region: string): string {
-    return region === this.regionActiva ? 'btn btn-primary': 'btn btn-outline-primary'
+  activarRegion( region: string ) {
+
+    if ( region === this.regionActiva ) { return; }
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion( region )
+      .subscribe( paises => this.paises = paises );
   }
 
 }
--- paisesApp/src/app/pais/services/pais.service.ts
+++ paisesApp/src/app/pais/services/pais.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Country } from '../interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +13,20 @@ export class PaisService {
   private apiUrl: string = 'https://restcountries.com/v2';
 
   get httpParams () {
-    return new HttpParams().set('fields', 'name,capital,alpha2Code,flag,population')
+    return new HttpParams().set( 'fields', 'name,capital,alpha2Code,flag,population' );
   }
-  
+
   constructor( private http: HttpClient ) { }
 
   buscarPais( termino: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ termino }`;
-    return this.http.get<Country[]>( url, {params: this.httpParams} );
+    
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
   buscarCapital( termino: string ):Observable<Country[]>{
     const url = `${ this.apiUrl }/capital/${ termino }`;
-    return this.http.get<Country[]>( url, {params: this.httpParams});
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
   getPaisPorAlpha( id: string ):Observable<Country>{
@@ -32,10 +34,14 @@ export class PaisService {
     return this.http.get<Country>( url );
   }
 
-  getCountriesByRegion(region: string): Observable<Country[]> {
+  buscarRegion( region: string ): Observable<Country[]> {
+
     const url = `${ this.apiUrl }/regionalbloc/${ region }`;
-    console.log('url'+url)
-    return this.http.get<Country[]>( url, {params: this.httpParams} );
+
+    return this.http.get<Country[]>( url, { params: this.httpParams } )
+            .pipe(
+              tap( console.log )
+            )
   }
 
 }
